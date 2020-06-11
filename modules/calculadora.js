aa27a7150f4f01a7663e34ade93a94f7154d7a7f
Lucas Vinotti <hp-lucas@hotmail.com>
2020-06-11 00:44:59 -0300

    agregue la funcion restar

--- modules/calculadora.js
+++ modules/calculadora.js
@@ -1,5 +1,8 @@
 let calculadora = {
     sumar: function (n1,n2){
-        return n1 + n2
-    }   
+        return n1 + n2;
+    },  
+    restar: function (n1,n2){
+        return n1 - n2;
+    },
 };
